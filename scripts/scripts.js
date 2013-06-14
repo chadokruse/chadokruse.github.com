023e78132e22d3a9002151921479e01f8c5dbb98
Chad Kruse <chadokruse@gmail.com>
2013-06-14 00:39:08 -0500

    Interim filter by animation fixes
    Would rather focus on filter-focus div instead of top of page. However,
    running into problem when social-ventures does not require a scroll (as
    it's too short)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,30 +6,42 @@ $(document).ready(function() {
     // Filter AJAX Call
     $('a.filter').on('click', function(event) {
 
-        // Highlight current subnav link
-        $('a.filter').removeClass('current'); // Remove current class from all subnav links
-        $(this).addClass('current'); // Add "current" class to clicked subnav link
-        $('body,html').animate({ // Scroll and focus on subnav at top TODO: still a bit jumpy when scrolls from bottom
-                scrollTop: 280
-            }, 'slow');
-        // Call new grid
+        var scrollToFilter = function(){  // Define Scroll-to-filter animation
+            $('body,html').animate({
+                scrollTop: 0
+                //scrollTop: $('#header').offset().top
+                }, "fast"); 
+        };
         var elem = $("#allposts");
         var url = $(this).attr('href');
+
+        // Highlight current subnav link
+        $('a.filter').removeClass('current');
+        $(this).addClass('current');
+
+        // Call new grid
          $.ajax({
             type: "GET",
             url: url,
             dataType: 'html',
+            beforeSend: function(){
+                $("#allposts").fadeOut("slow");
+                scrollToFilter(); 
+            },
             success: function(data) {
-                
-                $(elem).replaceWith(data);
-                arrange(); // Call grid-a-licious to calculate new layout 
-                 
-                $(".eachpost").css({opacity: 0});
-                $(".eachpost").fadeTo("slow", 1);                                     
+                $(elem).replaceWith(function() {
+                    return $(data).hide().fadeIn();
+                });
+                //$(elem).replaceWith(data).delay(2000);
+                arrange(); // Call grid-a-licious to calculate new layout                                   
+             },
+             complete: function(){
+                $("#allposts").css({opacity: 0});
+                $("#allposts").fadeTo("slow", 1);  
              },
          }); 
          event.preventDefault();
-         //event.stopPropagation();
+         event.stopPropagation();
     });
 
     // Hover gray-out effect
