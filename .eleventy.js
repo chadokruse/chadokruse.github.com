1418b67eda107819f4f91765ba4a3c683c4fcd69
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2022-03-04 12:47:57 -0600

    Fix resume css path

--- .eleventy.js
+++ .eleventy.js
@@ -18,11 +18,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/images');
   eleventyConfig.addPassthroughCopy('./src/js');
   eleventyConfig.addPassthroughCopy('./CNAME');
-  eleventyConfig.addPassthroughCopy({'./node_modules/flowbite/dist/flowbite.js': './js/flowbite.js'});
+  eleventyConfig.addPassthroughCopy({ './node_modules/flowbite/dist/flowbite.js': './js/flowbite.js' });
+  eleventyConfig.addPassthroughCopy({ './src/css/resume.css': './css/resume.css' });
 
   // Watch for css and config changes
   eleventyConfig.addWatchTarget('./tailwind.config.js');
   eleventyConfig.addWatchTarget('./src/css/main.css');
+  eleventyConfig.addWatchTarget('./src/css/resume.css');
 
   return {
     dir: {
