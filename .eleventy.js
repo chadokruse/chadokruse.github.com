6c7a024d0f6748e26570cf127891eabad0a390d6
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2022-03-08 09:54:28 -0600

    Favicon improvements

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,10 @@ module.exports = function(eleventyConfig) {
   });
 
   // Passthrough static items
+  eleventyConfig.addPassthroughCopy('./src/safari-pinned-tab.svg');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('./src/favicon-32x32.png');
+  eleventyConfig.addPassthroughCopy('./src/favicon-16x16.png');
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
   eleventyConfig.addPassthroughCopy('./src/robots.txt');
   eleventyConfig.addPassthroughCopy('./src/images');
