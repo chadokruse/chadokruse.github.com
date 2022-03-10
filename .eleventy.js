3b15d06b6292a48969fc07ce95bcbfb8d79c06e3
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2022-03-10 10:21:14 -0600

    Simplify file structure

--- .eleventy.js
+++ .eleventy.js
@@ -13,17 +13,11 @@ module.exports = function(eleventyConfig) {
   });
 
   // Passthrough static items
-  eleventyConfig.addPassthroughCopy('./src/safari-pinned-tab.svg');
-  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
-  eleventyConfig.addPassthroughCopy('./src/favicon-32x32.png');
-  eleventyConfig.addPassthroughCopy('./src/favicon-16x16.png');
-  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('./src/robots.txt');
+  // eleventyConfig.addPassthroughCopy({ './node_modules/flowbite/dist/flowbite.js': './js/flowbite.js' });
+  eleventyConfig.addPassthroughCopy({ './src/css/resume.css': './css/resume.css' });
+  eleventyConfig.addPassthroughCopy({ './src/static': '.'});
   eleventyConfig.addPassthroughCopy('./src/images');
   eleventyConfig.addPassthroughCopy('./src/js');
-  eleventyConfig.addPassthroughCopy('./CNAME');
-  eleventyConfig.addPassthroughCopy({ './node_modules/flowbite/dist/flowbite.js': './js/flowbite.js' });
-  eleventyConfig.addPassthroughCopy({ './src/css/resume.css': './css/resume.css' });
 
   // Watch for css and config changes
   eleventyConfig.addWatchTarget('./tailwind.config.js');
